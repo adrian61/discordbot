5792ed3ec4486b02c74006d6c8b7b0499424db38
adrian61 <ad.kowalskI@gmail.com>
2020-05-27 21:03:18 +0200

    Added changing server regions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const Discord = require('discord.js');
 const commandDisconnect = require('./command/disconnect.js');
 const commandHelp = require('./command/help.js');
@@ -50,20 +51,33 @@ client.on('message', message => {
     }
     if (command === 'serverlist') {
         let channel = client.channels.find(ch => ch.name === 'main');
-        channel.send(message.guild.fetchVoiceRegions());
+        message.guild.fetchVoiceRegions()
+            .then(regions => channel.send(`Available regions are: ${regions.map(region => region.name).join(', ')}`))
     }
-});
+    if (command === 'setserver') {
+        let channel = client.channels.find(ch => ch.name === 'main');
+        if (args.length == 2) {
+            channel.send("Value must be one of ('dubai', 'amsterdam', 'us-central', 'hongkong'," +
+                " 'south-korea', 'us-east', 'russia', 'southafrica', 'brazil', 'us-west', 'sydney'," +
+                " 'japan', 'us-south', 'europe', 'eu-central', 'singapore', 'eu-west', 'london', 'india'," +
+                " 'frankfurt'");
+        }
+        if (args.length == 1) {
+            message.guild.setRegion(args[0]).then(channel.send("Changed server to: " + args[0] + ".")).catch(err => channel.send(err));
+        }
+    }
+})
+;
 client.on('voiceStateUpdate', (oldMember, newMember) => {
     let newUserChannel = newMember.voiceChannel;
     let oldUserChannel = oldMember.voiceChannel;
     if (oldUserChannel === undefined && newUserChannel !== undefined) {
-        let channel = client.channels.find(ch => ch.name === 'general');
+        let channel = client.channels.find(ch => ch.name === 'logs');
         channel.send('<@' + newMember.user.id + ">" + ' joined to the Voice Channel ' + newMember.guild.member(newMember.user.id).voiceChannel.name);
     } else if (newUserChannel === undefined) {
-        let channel = client.channels.find(ch => ch.name === 'general');
+        let channel = client.channels.find(ch => ch.name === 'logs');
         channel.send('<@' + newMember.user.id + ">" + ' disconnected from the server. ');
     }
 });
-
 
 client.login(token);
